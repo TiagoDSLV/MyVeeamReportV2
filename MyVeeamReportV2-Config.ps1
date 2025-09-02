14448e5d7b9cbf6b4799a4af68a6dd8df52018d9
Tiago DA SILVA <tiago.da-silva@atheo.net>
2025-09-02 15:51:31 +0200

    Update MyVeeamReportV2-Config.ps1

--- MyVeeamReportV2-Config.ps1
+++ MyVeeamReportV2-Config.ps1
@@ -1,6 +1,6 @@
 <#====================================================================
 Author        : Tiago DA SILVA - ATHEO INGENIERIE
-Version       : 1.0.4
+Version       : 1.0.5
 Creation Date : 2025-07-01
 Last Update   : 2025-07-01
 GitHub Repo   : https://github.com/TiagoDSLV/MyVeeamReportV2/
